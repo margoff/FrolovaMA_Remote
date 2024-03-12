6e318718d4f382a60497c9601235f227e1837bec
Маргарита <margo22010f@gmail.com>
2024-03-12 21:41:13 +0300

    add new feature

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿namespace FrolovaMA
 {
-    internal class Program
+    using System;
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            double number = 5.0;
+            double result = Sqr(number);
+            Console.WriteLine($"Квадрат числа {number} равен {result}");
         }
+
+        static public double Sqr(double X)
+        {
+            return X * X;
+        }
+        // Frolova Margarita Alekseevna ZAm-1-23
+        //<//solve task2>
     }
-    // Frolova Margarita Alekseevna ZAm-1-23
-    //<//solve task2>
+    
 }
