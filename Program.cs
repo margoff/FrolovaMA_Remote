e3709f9862ca12119e5d14f2a49c749863bc4ac1
Маргарита <margo22010f@gmail.com>
2024-03-14 20:31:31 +0300

    Solve conflict new_feature & task1_ФроловаМА

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static public double Sqr(double X)
+        {
+            return X * X;
+        }
+        // Frolova Margarita Alekseevna ZAm-1-23
+        //<//solve task2>
+        //solve task1
+    }
+
     }
     // Frolova Margarita Alekseevna ZAm-1-23
     //<//solve task2>
